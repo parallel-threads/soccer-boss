1ab5621fc94a0692860fe8ac6934fd429fdc3b5b
Parallel Threads Admin <admin@parallel-threads.com>
2013-07-18 08:05:03 -0600

    Removed the submitted div from the node template.

--- sites/all/themes/andromeda/templates/node.tpl.php
+++ sites/all/themes/andromeda/templates/node.tpl.php
@@ -19,13 +19,13 @@
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
-
+<!--
   <?php if ($display_submitted): ?>
     <div class="submitted">
       <?php print $submitted; ?>
     </div>
   <?php endif; ?>
-
+-->
   <div class="content"<?php print $content_attributes; ?>>
     <?php
       // We hide the comments and links now so that we can render them later.
